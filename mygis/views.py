58f2af1beeba4aacad39a409f69f903ee1f7d182
Bohdan Savchuk <pirkul132@gmail.com>
2024-03-12 20:17:28 +0200

    view home was modified(articles context). tepmlate home was modified(displayed articles)

--- mygis/views.py
+++ mygis/views.py
@@ -1,8 +1,18 @@
 from django.http import HttpResponse
 from django.shortcuts import render
 
+from mygis.models import Articles
+
 
 # Create your views here.
 def home(request):
-    # return HttpResponse("Hello World")
-    return render(request, 'mygis/home.html')
+    # return HttpResponse("Hello world!")
+    # return render(request, 'mygis/home.html')
+    articles = Articles.objects.all()
+    context = {
+        'articles': articles
+    }
+    return render(request, 'mygis/home.html', context)
+
+
+
